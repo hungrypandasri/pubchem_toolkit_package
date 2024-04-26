dbde081983d4bff8694635b5e037f97319b19af8
Sriram Natarajan <144051637+hungrypandasri@users.noreply.github.com>
2024-04-26 19:15:17 -0400

    Update __innit__.py

--- PubChem V2/pubchem_toolkit/__innit__.py
+++ PubChem V2/pubchem_toolkit/__innit__.py
@@ -1,7 +1,11 @@
-# File: pubchem_toolkit/__init__.py
+"""
+PubChem Toolkit: A toolkit for accessing PubChem database.
+
+This module provides functions for interacting with the PubChem database.
+"""
 
 # Import commonly used modules or functions
-from .compound_search import (
-    autocomplete_search,
-    get_compound_properties_with_autocomplete,
-)
+from .modified_compound_search import autocomplete_search, get_compound_properties_with_autocomplete
+
+# Export the functions to make them available when importing the package
+__all__ = ["autocomplete_search", "get_compound_properties_with_autocomplete"]
